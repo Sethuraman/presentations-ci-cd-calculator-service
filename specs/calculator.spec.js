3c11220bb6489122ee3544edaf73c62dabd9dbee
Bhavi <jagwani.bhavi@gmail.com>
2018-02-02 17:30:30 +0530

    Adding API tests

--- specs/calculator.spec.js
+++ specs/calculator.spec.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const Calculator = require('./calculator');
+const Calculator = require('../calculator');
 const chai = require('chai')
 const assert = chai.assert;
 
 describe("Calculator test", function() {
 
-	it("should correctly add two numbers", function(){
+	it("correctly adds two numbers", function(){
 		assert.equal(Calculator.add(4, 5), 9);
 	});
 
-	it("should correctly subtract two numbers", function(){
+	it("correctly subtracts two numbers", function(){
 		assert.equal(Calculator.subtract(8, 3), 5);
 	});
 
-	it("should correctly multiply two numbers", function(){
+	it("correctly multiplies two numbers", function(){
 		assert.equal(Calculator.multiply(4, 5), 20);
 	});
 });
---
+++ specs/handler.spec.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const chai = require('chai')
+const assert = chai.assert;
+const should = require('chai').should();
+
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const URL = process.env.CALCULATION_SERVICE_URL;
+
+describe('Calculator', function() {
+	it('GET /add', function(done){
+		chai.request(URL)
+		.get('/add')
+		.query({number1: 5, number2: 7})
+		.end(function(err, res) {
+			should.not.exist(err);
+			res.should.have.status(200);
+			assert.equal(res.body.result, 12);
+			done();
+		});
+	});
+
+	it('GET /subtract', function(done){
+		chai.request(URL)
+		.get('/subtract')
+		.query({number1: 5, number2: 7})
+		.end(function(err, res) {
+			should.not.exist(err);
+			res.should.have.status(200);
+			assert.equal(res.body.result, -2);
+			done();
+		});
+	});
+
+	it('GET /multiply', function(done){
+		chai.request(URL)
+		.get('/multiply')
+		.query({number1: 5, number2: 7})
+		.end(function(err, res) {
+			should.not.exist(err);
+			res.should.have.status(200);
+			assert.equal(res.body.result, 35);
+			done();
+		});
+	});
+});
